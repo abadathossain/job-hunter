edbe099ee46b5a85670ed23a60aa8fd930cee246
Abadat Hossain <abadat26@gmail.com>
2025-03-18 01:26:07 +0600

    header and footer done

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom"
 import Home from "../components/Home/Home"
 import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header"
 
 const Root = () => {
     return (
         <div>
-            <Home></Home>
-            <Outlet></Outlet>
+            <Header></Header>
+            <div className="min-h-screen">
+                <Outlet></Outlet>
+            </div>
             <Footer></Footer>
 
         </div>
